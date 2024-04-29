55c55d786bb3dd4d500a0d457a9695b7b6a6bad4
MATHEUS SANTOS PEREIRA <021.898669@senaicimatec.edu.br>
2024-04-29 18:01:30 -0300

    matheus

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import Nav from './components/Nav'
+import Footer from './components/Footer';
+import batata from './components/batata';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      
+      <Footer/>
+      
+
+
+    </div>
+  );
+}
+
+export default App;
